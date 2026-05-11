23343036451ba5c132a998ce2ecd26a8a265821c
agiodesign <agioid@agiodesign.co.kr>
2026-05-11 20:37:13 +0900

    Update building.js

--- api/building.js
+++ api/building.js
@@ -1,11 +1,9 @@
 const BUILDING_API_KEY = 'mX7nd4kSo5SUrlBEp4FVl2NqZIORFTqKdRXaY2Z2dGwTMIGpEkWDOlwl2YhJHZMx1ED5HpzMDBj4PFY05iA9vQ=='
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-
   const { type, sigunguCd, bjdongCd, bun, ji } = req.query
   const endpoint = type === 'floor' ? 'getBrFlrOulnInfo' : 'getBrTitleInfo'
-
   const params = new URLSearchParams({
     serviceKey: BUILDING_API_KEY,
     sigunguCd,
@@ -16,11 +14,8 @@ export default async function handler(req, res) {
     numOfRows: type === 'floor' ? '100' : '10',
     _type: 'json',
   })
-
   try {
-    const response = await fetch(
-      `https://apis.data.go.kr/1613000/BldRgstHubService/${endpoint}?${params}`
-    )
+    const response = await fetch(`https://apis.data.go.kr/1613000/BldRgstHubService/${endpoint}?${params}`)
     const data = await response.json()
     res.status(200).json(data)
   } catch (e) {
